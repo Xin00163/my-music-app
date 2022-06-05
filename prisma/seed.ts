981f71f15e042208cbb14d460cc620a4f0e5eed4
Xin00163 <wangxin00163@yahoo.co.uk>
2022-06-05 14:52:42 +0100

    feat: playlist ui

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -31,8 +31,8 @@ const run = async () => {
     create: {
       email: "user@test.com",
       password: bcrypt.hashSync("password", salt),
-      firstName: 'Xin',
-      lastName: 'Wang',
+      firstName: "Xin",
+      lastName: "Wang",
     },
   });
 
